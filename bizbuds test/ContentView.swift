58c0f256660cb4467fff0cee1b3cc500053682c2
Shielda <e1075544@u.nus.edu>
2023-06-01 22:50:09 +0700

    Initial Commit

---
+++ bizbuds test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  bizbuds test
+//
+//  Created by Shielda Hidajat on 01/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bizbuds test/bizbuds_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bizbuds_testApp.swift
+//  bizbuds test
+//
+//  Created by Shielda Hidajat on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct bizbuds_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
